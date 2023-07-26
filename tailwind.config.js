2e186dfd81a4f650eacda84c1d56fa1cdc7ec676
Ray Mayorquin <raymundoeduardo@outlook.com>
2023-07-26 15:43:12 -0700

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-1': '#495e57',
+        'primary-2': '#f4ce14',
+        'secondary-1': '#ee9972',
+        'secondary-2': '#fbdabb',
+        light: '#edefee',
+        dark: '#333',
+      },
+    },
+  },
+  plugins: [],
+};
